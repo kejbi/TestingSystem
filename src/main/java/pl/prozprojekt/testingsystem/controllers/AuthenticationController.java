809c0582e9b8f29be8e138d69a332d496b7b22f2
Arton <arton-10@o2.pl>
2019-05-20 13:39:14 +0200

    Added payloads - LoginRequest ad JwtAuthenticationResponse

--- src/main/java/pl/prozprojekt/testingsystem/controllers/AuthenticationController.java
+++ src/main/java/pl/prozprojekt/testingsystem/controllers/AuthenticationController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.prozprojekt.testingsystem.payload.JwtAuthenticationResponse;
+import pl.prozprojekt.testingsystem.payload.LoginRequest;
 import pl.prozprojekt.testingsystem.security.JwtTokenProvider;
 
 import javax.validation.Valid;
