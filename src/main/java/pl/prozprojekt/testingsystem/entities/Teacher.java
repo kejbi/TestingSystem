0237514ade7de323f0517c9bc8c3b723b269c08f
PawelKurowski <pawelk0@gazeta.pl>
2019-05-21 11:33:08 +0200

    Teacher entity changed - previous variables removed, added quizzes list

--- src/main/java/pl/prozprojekt/testingsystem/entities/Teacher.java
+++ src/main/java/pl/prozprojekt/testingsystem/entities/Teacher.java
@@ -8,23 +8,13 @@ import java.util.List;
 @Entity
 public class Teacher extends User {
 
-    private int test; //put here so the entity wont be empty, almost certainly removed or changed but might be useful
+    private List<Quiz> quizzes;
 
-    private int difficulty; //difficulty of test, might be used later to pass to test generating function
-
-    public int getTest() {
-        return test;
+    public List<Quiz> getQuizzes() {
+        return quizzes;
     }
 
-    public void setTest(int test) {
-        this.test = test;
-    }
-
-    public int getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(int difficulty) {
-        this.difficulty = difficulty;
+    public void setQuizzes(List<Quiz> quizzes) {
+        this.quizzes = quizzes;
     }
 }
