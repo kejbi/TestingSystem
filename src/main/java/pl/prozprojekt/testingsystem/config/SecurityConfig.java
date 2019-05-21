d69836c11444884ce24de160c6537e58dc356665
kejbi <biegajski.kacper@gmail.com>
2019-05-21 22:51:51 +0200

    config package, webmvcconfiguration (cors)

--- src/main/java/pl/prozprojekt/testingsystem/config/SecurityConfig.java
+++ src/main/java/pl/prozprojekt/testingsystem/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pl.prozprojekt.testingsystem;
+package pl.prozprojekt.testingsystem.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
